ec3f598e6e7d9d0696b7359ce0ffd86010cd0e0c
Vitali Lucenco <vitali.lucenco@mttnow.com>
2016-07-17 15:41:40 +0300

    [Lesson 1, Task №2] Added function to calculate distance between 2 points
    Выполнил пункты 1, 2 и 3 из задания №2

--- sandbox/src/main/java/ru/vitali/pft/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/vitali/pft/sandbox/MyFirstProgram.java
@@ -2,7 +2,15 @@ package ru.vitali.pft.sandbox;
 
 public class MyFirstProgram {
 
-    public static void main(String[] args) {
-        System.out.println("Hello, World!");
-    }
+  public static void main(String[] args) {
+
+    Point p1 = new Point(1, 4);
+    Point p2 = new Point(3, 5);
+    System.out.println("Расстояние между точками " +
+            "А(" + p1.x + ";" + p1.y + ") и B(" + p2.x + ";" + p2.y + ")" + " = " + distance(p1, p2));
+  }
+
+  public static double distance(Point p1, Point p2) {
+    return Math.sqrt(Math.pow((p2.x - p1.x), 2) + Math.pow((p2.y - p1.y), 2));
+  }
 }
