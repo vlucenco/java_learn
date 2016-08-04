d3c785e5fa582232bab7f18b33ae831860bae9da
Vitali Lucenco <lucenco.v@gmail.com>
2016-08-04 10:17:46 +0300

    Provide fulfilment of preconditions for Group tests
    Adds checking and fulfilment of preconditions for these tests:
    * GroupCreationTests
    * GroupModificationTests

--- addressbook-web-tests/src/test/java/ru/vitali/pft/addressbook/tests/GroupCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/vitali/pft/addressbook/tests/GroupCreationTests.java
@@ -9,9 +9,6 @@ public class GroupCreationTests extends TestBase {
   public void testGroupCreation() {
 
     app.getNavigationHelper().gotoGroupPage();
-    app.getGroupHelper().initGroupCreation();
-    app.getGroupHelper().fillGroupForm(new GroupData("test1", "test2", "test3"));
-    app.getGroupHelper().submitGroupCreation();
-    app.getGroupHelper().returnToGroupPage();
+    app.getGroupHelper().createGroup(new GroupData("test1", "test2", "test3"));
   }
 }
--- addressbook-web-tests/src/test/java/ru/vitali/pft/addressbook/tests/GroupDeletionTests.java
+++ addressbook-web-tests/src/test/java/ru/vitali/pft/addressbook/tests/GroupDeletionTests.java
@@ -1,12 +1,16 @@
 package ru.vitali.pft.addressbook.tests;
 
 import org.testng.annotations.Test;
+import ru.vitali.pft.addressbook.model.GroupData;
 
 public class GroupDeletionTests extends TestBase {
 
   @Test
   public void testGroupDeletion() {
     app.getNavigationHelper().gotoGroupPage();
+    if (!app.getGroupHelper().isThereAGroup()){
+      app.getGroupHelper().createGroup(new GroupData("test1", null, null));
+    }
     app.getGroupHelper().selectGroup();
     app.getGroupHelper().deleteSelectedGroups();
     app.getGroupHelper().returnToGroupPage();
