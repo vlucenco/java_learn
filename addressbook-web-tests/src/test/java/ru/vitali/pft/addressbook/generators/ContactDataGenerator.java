d6ec6afe694f7ceb39584c2e868b87e1953ac9bf
Vitali Lucenco <lucenco.v@gmail.com>
2016-08-30 09:10:00 +0300

    [Task 14] Adds ContactDataGenerator supporting csv, xml and json formats
    To generate data pass Parameters of ContactDataGenerator to Program
    Arguments and run the class, i.e.:
    Go to ContactDataGenerator -> Edit Configurations -> Program Arguments:
    -f src/test/resources/contacts.json -c 3 -d json

---
+++ addressbook-web-tests/src/test/java/ru/vitali/pft/addressbook/generators/ContactDataGenerator.java
@@ -0,0 +1,96 @@
+package ru.vitali.pft.addressbook.generators;
+
+import com.beust.jcommander.JCommander;
+import com.beust.jcommander.Parameter;
+import com.beust.jcommander.ParameterException;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.thoughtworks.xstream.XStream;
+import ru.vitali.pft.addressbook.model.ContactData;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.Writer;
+import java.util.ArrayList;
+import java.util.List;
+
+public class ContactDataGenerator {
+
+  @Parameter(names = "-c", description = "Contact count")
+  public int count;
+  @Parameter(names = "-f", description = "Target file")
+  public String file;
+  @Parameter(names = "-d", description = "Data format")
+  public String format;
+
+  public static void main(String[] args) throws IOException {
+    ContactDataGenerator generator = new ContactDataGenerator();
+    JCommander jCommander = new JCommander(generator);
+    try {
+      jCommander.parse(args);
+    } catch (ParameterException ex) {
+      jCommander.usage();
+      return;
+    }
+    generator.run();
+  }
+
+  private void run() throws IOException {
+    List<ContactData> contacts = generateContacts(count);
+    if (format.equals("csv")) {
+      saveAsCsv(contacts, new File(file));
+    } else if (format.equals("xml")) {
+      saveAsXml(contacts, new File(file));
+    } else if (format.equals("json")) {
+      saveAsJson(contacts, new File(file));
+    } else {
+      System.out.println("Unrecognized format " + format);
+    }
+  }
+
+  private void saveAsJson(List<ContactData> contacts, File file) throws IOException {
+    Gson gson = new GsonBuilder().setPrettyPrinting().create();
+    String json = gson.toJson(contacts);
+    Writer writer = new FileWriter(file);
+    writer.write(json);
+    writer.close();
+  }
+
+  private void saveAsXml(List<ContactData> contacts, File file) throws IOException {
+    XStream xstream = new XStream();
+    String xml = xstream.toXML(contacts);
+    Writer writer = new FileWriter(file);
+    writer.write(xml);
+    writer.close();
+  }
+
+  private void saveAsCsv(List<ContactData> contacts, File file) throws IOException {
+    System.out.println(new File(".").getAbsolutePath());
+    Writer writer = new FileWriter(file);
+    for (ContactData contact : contacts) {
+      writer.write(String.format("%s;%s;%s;%s;%s;%s;%s;%s;%s\n",
+              contact.getFirstName(), contact.getLastName(), contact.getAddress(),
+              contact.getHomePhone(), contact.getMobilePhone(), contact.getWorkPhone(),
+              contact.getEmail1(), contact.getEmail2(), contact.getEmail3()));
+    }
+    writer.close();
+  }
+
+  private List<ContactData> generateContacts(int count) {
+    List<ContactData> contacts = new ArrayList<ContactData>();
+    for (int i = 0; i < count; i++) {
+      contacts.add(new ContactData()
+              .withFirstName(String.format("First Name %s", i))
+              .withLastName(String.format("Last Name %s", i))
+              .withAddress(String.format("Address %s", i))
+              .withHomePhone(String.format("Home Phone %s", i))
+              .withMobilePhone(String.format("Mobile Phone %s", i))
+              .withWorkPhone(String.format("Work Phone %s", i))
+              .withEmail1(String.format("Email1 %s", i))
+              .withEmail2(String.format("Email2 %s", i))
+              .withEmail3(String.format("Email3 %s", i)));
+    }
+    return contacts;
+  }
+}
