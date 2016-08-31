6ed2a79a08290be280f55ef0d6f4157a524fce3f
Vitali Lucenco <lucenco.v@gmail.com>
2016-08-31 13:07:56 +0300

    Configure browser type from IDE
    * Go to Edit configurations of a test class and pass browser type to VM
    options, i.e.:
    VM options: -ea -Dbrowser=firefox
    * If nothing passed to VM options, then default browser is used - the
    second param in System.getProperty()

--- addressbook-web-tests/src/test/java/ru/vitali/pft/addressbook/tests/TestBase.java
+++ addressbook-web-tests/src/test/java/ru/vitali/pft/addressbook/tests/TestBase.java
@@ -9,7 +9,8 @@ import ru.vitali.pft.addressbook.appmanager.ApplicationManager;
 
 public class TestBase {
 
-  protected static final ApplicationManager app = new ApplicationManager(BrowserType.CHROME);
+  protected static final ApplicationManager app
+          = new ApplicationManager(System.getProperty("browser", BrowserType.CHROME));
 
   @BeforeSuite
   public void setUp() throws Exception {
