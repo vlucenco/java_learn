81df86b0c92625c325bcbc5884957f302e451b34
Vitali Lucenco <lucenco.v@gmail.com>
2016-09-05 21:24:17 +0300

    Creates HttpSession helper and testLogin test

---
+++ mantis-tests/src/test/java/ru/vitali/pft/mantis/tests/LoginTests.java
@@ -0,0 +1,18 @@
+package ru.vitali.pft.mantis.tests;
+
+import org.testng.annotations.Test;
+import ru.vitali.pft.mantis.appmanager.HttpSession;
+
+import java.io.IOException;
+
+import static org.testng.Assert.assertTrue;
+
+public class LoginTests extends TestBase {
+
+  @Test
+  public void testLogin() throws IOException {
+    HttpSession session = app.newSession();
+    assertTrue(session.login("administrator", "root"));
+    assertTrue(session.isLoggedInAs("administrator"));
+  }
+}
