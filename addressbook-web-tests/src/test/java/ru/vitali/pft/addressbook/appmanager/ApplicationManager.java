c3d8cc3a08f8ea865749b55a20096151f821dc30
Vitali Lucenco <lucenco.v@gmail.com>
2016-09-18 11:06:05 +0300

    Implements possibility to specify platform to run tests on

--- addressbook-web-tests/src/test/java/ru/vitali/pft/addressbook/appmanager/ApplicationManager.java
+++ addressbook-web-tests/src/test/java/ru/vitali/pft/addressbook/appmanager/ApplicationManager.java
@@ -1,5 +1,6 @@
 package ru.vitali.pft.addressbook.appmanager;
 
+import org.openqa.selenium.Platform;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
@@ -48,6 +49,7 @@ public class ApplicationManager {
     } else {
       DesiredCapabilities capabilities = new DesiredCapabilities();
       capabilities.setBrowserName(browser);
+      capabilities.setPlatform(Platform.fromString(System.getProperty("platform", "mac")));
       wd = new RemoteWebDriver(new URL(properties.getProperty("selenium.server")), capabilities);
     }
     wd.manage().timeouts().implicitlyWait(0, TimeUnit.SECONDS);
